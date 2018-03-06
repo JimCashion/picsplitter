c689fbf7244e8c036ec1dd0aa7ef140cd153e888
Unknown <jim.cashion@hotmail.co.uk>
2018-03-06 15:26:45 +0000

    Loading full project

---
+++ picsplitter/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace picsplitter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pborig = new System.Windows.Forms.PictureBox();
+            this.cbtype = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pborig)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pborig
+            // 
+            this.pborig.Location = new System.Drawing.Point(50, 50);
+            this.pborig.Name = "pborig";
+            this.pborig.Size = new System.Drawing.Size(100, 50);
+            this.pborig.TabIndex = 0;
+            this.pborig.TabStop = false;
+            // 
+            // cbtype
+            // 
+            this.cbtype.FormattingEnabled = true;
+            this.cbtype.Items.AddRange(new object[] {
+            "Pick a size",
+            "4x4",
+            "5x5",
+            "6x6",
+            "7x7"});
+            this.cbtype.Location = new System.Drawing.Point(36, 12);
+            this.cbtype.Name = "cbtype";
+            this.cbtype.Size = new System.Drawing.Size(121, 24);
+            this.cbtype.TabIndex = 1;
+            this.cbtype.Text = "Pick a size";
+            this.cbtype.SelectedIndexChanged += new System.EventHandler(this.cbtype_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1850, 818);
+            this.Controls.Add(this.cbtype);
+            this.Controls.Add(this.pborig);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pborig)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pborig;
+        private System.Windows.Forms.ComboBox cbtype;
+    }
+}
+
---
+++ picsplitter/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace picsplitter
+{
+    public partial class Form1 : Form
+    {
+        string path4x4 = @"C:\jim\picsplitter\pics\4x4\";
+        string path5x5 = @"C:\jim\picsplitter\pics\5x5\";
+        string path6x6 = @"C:\jim\picsplitter\pics\6x6\";
+        string path7x7 = @"C:\jim\picsplitter\pics\7x7\";
+        Image mainimage;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void cbtype_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cbtype.Text)
+            {
+                case "4x4":
+                    pborig.Size = new Size(352, 352);
+                    mainimage = Image.FromFile(path4x4 + cbtype.Text + ".png");
+                    pborig.Image = mainimage;
+
+                    do4x4();
+                    break;
+                case "5x5":
+                    pborig.Size = new Size(500, 500);
+                    mainimage = Image.FromFile(path5x5 + cbtype.Text + ".png");
+                    pborig.Image = mainimage;
+
+                    do5x5();
+                    break;
+                case "6x6":
+                    pborig.Size = new Size(480, 480);
+                    mainimage = Image.FromFile(path6x6 + cbtype.Text + ".png");
+                    pborig.Image = mainimage;
+
+                    do6x6();
+                    break;
+                case "7x7":
+                    pborig.Size = new Size(490, 490);
+                    mainimage = Image.FromFile(path7x7 + cbtype.Text + ".png");
+                    pborig.Image = mainimage;
+
+                    do7x7();
+                    break;
+            }
+        }
+
+        private void do7x7()
+        {
+            this.Width = 3 * 490 + 150;
+
+            for (int x = 0;x<7;x++)
+            {
+                for (int y = 0; y < 7; y++)
+                {
+                    PictureBox pb1 = new PictureBox();
+                    pb1.Size = new Size(70, 70);
+                    pb1.Location = new Point(540 + x * 70, 50 + y * 70);
+
+                    PictureBox pb2 = new PictureBox();
+                    pb2.Size = new Size(70, 70);
+                    pb2.Location = new Point(1080 + x * 70, 50 + y * 70);
+
+                    //pb.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                    Bitmap bmp = new Bitmap(path7x7 + cbtype.Text + ".png");
+                    Bitmap piece = bmp.Clone(new Rectangle(x * 70, y * 70, 70, 70), System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+                    
+                    Image i = piece;
+                    pb1.Image = i;
+
+                    string newpicpath = path7x7 + "i" + x.ToString() + "x" + y.ToString() + ".png";
+                    piece.Save(newpicpath, System.Drawing.Imaging.ImageFormat.Png);
+
+                    pb2.Image = Image.FromFile(newpicpath);
+
+                    this.Controls.Add(pb1);
+                    this.Controls.Add(pb2);
+                }
+            }
+        }
+
+        private void do6x6()
+        {
+            this.Width = 3 * 480 + 150;
+
+            for (int x = 0; x < 6; x++)
+            {
+                for (int y = 0; y < 6; y++)
+                {
+                    PictureBox pb1 = new PictureBox();
+                    pb1.Size = new Size(80, 80);
+                    pb1.Location = new Point(530 + x * 80, 50 + y * 80);
+
+                    PictureBox pb2 = new PictureBox();
+                    pb2.Size = new Size(80, 80);
+                    pb2.Location = new Point(1060 + x * 80, 50 + y * 80);
+
+                    //pb.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                    Bitmap bmp = new Bitmap(path6x6 + cbtype.Text + ".png");
+                    Bitmap piece = bmp.Clone(new Rectangle(x * 80, y * 80, 80, 80), System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+                    Image i = piece;
+                    pb1.Image = i;
+
+                    string newpicpath = path6x6 + "i" + x.ToString() + "x" + y.ToString() + ".png";
+                    piece.Save(newpicpath, System.Drawing.Imaging.ImageFormat.Png);
+
+                    pb2.Image = Image.FromFile(newpicpath);
+
+                    this.Controls.Add(pb1);
+                    this.Controls.Add(pb2);
+                }
+            }
+        }
+
+        private void do5x5()
+        {
+            this.Width = 3 * 500 + 150;
+
+            for (int x = 0; x < 5; x++)
+            {
+                for (int y = 0; y < 5; y++)
+                {
+                    PictureBox pb1 = new PictureBox();
+                    pb1.Size = new Size(100, 100);
+                    pb1.Location = new Point(550 + x * 100, 50 + y * 100);
+
+                    PictureBox pb2 = new PictureBox();
+                    pb2.Size = new Size(100, 100);
+                    pb2.Location = new Point(1100 + x * 100, 50 + y * 100);
+
+                    //pb.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                    Bitmap bmp = new Bitmap(path5x5 + cbtype.Text + ".png");
+                    Bitmap piece = bmp.Clone(new Rectangle(x * 100, y * 100, 100, 100), System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+                    Image i = piece;
+                    pb1.Image = i;
+
+                    string newpicpath = path5x5 + "i" + x.ToString() + "x" + y.ToString() + ".png";
+                    piece.Save(newpicpath, System.Drawing.Imaging.ImageFormat.Png);
+
+                    pb2.Image = Image.FromFile(newpicpath);
+
+                    this.Controls.Add(pb1);
+                    this.Controls.Add(pb2);
+                }
+            }
+        }
+        private void do4x4()
+        {
+            this.Width = 3 * 352 + 150;
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 4; y++)
+                {
+                    PictureBox pb1 = new PictureBox();
+                    pb1.Size = new Size(88, 88);
+                    pb1.Location = new Point(400 + x * 88, 50 + y * 88);
+
+                    PictureBox pb2 = new PictureBox();
+                    pb2.Size = new Size(88, 88);
+                    pb2.Location = new Point(800 + x * 88, 50 + y * 88);
+
+                    //pb.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                    Bitmap bmp = new Bitmap(path4x4 + cbtype.Text + ".png");
+                    Bitmap piece = bmp.Clone(new Rectangle(x * 88, y * 88, 88, 88), System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+                    Image i = piece;
+                    pb1.Image = i;
+
+                    string newpicpath = path4x4 + "i" + x.ToString() + "x" + y.ToString() + ".png";
+                    piece.Save(newpicpath, System.Drawing.Imaging.ImageFormat.Png);
+
+                    pb2.Image = Image.FromFile(newpicpath);
+
+                    this.Controls.Add(pb1);
+                    this.Controls.Add(pb2);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbtype_SelectedIndexChanged(null, null);
+        }
+    }
+}
